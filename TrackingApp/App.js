4e219d7d646195a1e105ebfe852d465ac02c3c5d
felixch10 <fchen2021@gmail.com>
2023-06-21 20:25:58 -0700

    Changed background color and added SafeAreaView.

--- TrackingApp/App.js
+++ TrackingApp/App.js
@@ -1,19 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Test Screen</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "dodgerblue",
     alignItems: "center",
     justifyContent: "center",
   },
