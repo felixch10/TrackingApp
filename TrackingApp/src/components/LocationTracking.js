a6047caaab791525aab14de7689afbeac00c17b1
felix <felixch@students.cs.ubc.ca>
2023-06-28 19:49:20 -0700

    fixed latitude and longitude

--- TrackingApp/src/components/LocationTracking.js
+++ TrackingApp/src/components/LocationTracking.js
@@ -5,26 +5,29 @@ import * as Location from "expo-location";
 const LocationTracking = () => {
   const [location, setLocation] = useState();
   const [latitude, setLatitude] = useState();
-  const [longtitude, setLongtitude] = useState();
+  const [longitude, setLongtitude] = useState();
+  const [country, setCountry] = useState();
 
   useEffect(() => {
     const getPermissions = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         //console.log("Please grant location permissions");
-        alert(message.alert)
+        //alert(message.alert);
         return;
       }
 
       let currentLocation = await Location.getCurrentPositionAsync({});
-      setLocation(currentLocation);
-      console.log("Latitude:", currentLocation.coords.latitude);
-      console.log("Longtitude:", currentLocation.coords.longitude);
+      const { latitude, longitude } = currentLocation.coords;
+      setLatitude(latitude);
+      setLongtitude(longitude);
+      console.log(latitude);
+      console.log(longitude);
     };
     getPermissions();
   }, []);
 
-  const reverseGeocode = 
+  const reverseGeocode = () => {};
 };
 
 export default LocationTracking;
